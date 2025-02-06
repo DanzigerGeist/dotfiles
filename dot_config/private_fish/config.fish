a793f6dd4c7d976620ac76fa5a56814c5729a215
Aleksy Rudy <aleksyrudy@MacBook-Pro-Aleksy.local>
2025-02-06 16:17:38 +0100

    Add .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    set -Ux fifc_editor nvim
+    set -U fish_greeting
+    set -gx OLLAMA_HOST http://localhost:11434
+    set -gx EDITOR nvim
+    set fish_tmux_autostart true
+
+    set HB_CNF_HANDLER (brew --prefix)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
+    if test -f $HB_CNF_HANDLER
+        source $HB_CNF_HANDLER
+    end
+
+    set PATH $PATH /Users/aleksyrudy/.local/bin
+
+    thefuck --alias | source
+
+    alias config="chezmoi edit --apply"
+end
