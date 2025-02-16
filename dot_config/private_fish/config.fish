a7a4b7a23bdf526bac48d16baad9541faf66e827
Aleksy Rudy <aleksy@aleksyrudy.pl>
2025-02-16 17:14:48 +0100

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,18 +1,24 @@
 if status is-interactive
+    # Setting up Fish shell first.
     set -U fish_greeting
-    set -gx OLLAMA_HOST http://localhost:11434
-    set -gx EDITOR nvim
     set fish_tmux_autostart true
+    set sponge_purge_only_on_exit true
 
+    # Homebrew's command-not-found handler.
     set HB_CNF_HANDLER (brew --prefix)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
     if test -f $HB_CNF_HANDLER
         source $HB_CNF_HANDLER
     end
 
+    # Other global variables.
     set PATH $PATH /Users/aleksyrudy/.local/bin
+    set OLLAMA_HOST http://localhost:11434
+    set EDITOR nvim
 
+    # Initializing shell utils.
     thefuck --alias | source
 
+    # Aliases.
     alias config="chezmoi edit --apply"
     alias telegram='nchat -d ~/.config/nchat/telegram'
     alias whatsapp='nchat -d ~/.config/nchat/whatsapp'
