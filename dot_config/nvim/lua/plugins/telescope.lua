ab844237b782fe37044b17344f9e8fb8ff76933a
Aleksy Rudy <aleksy@aleksyrudy.pl>
2025-02-20 13:37:45 +0100

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,5 +2,14 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim'
-  }
+  },
+  config = function()
+    require "telescope".setup {
+      pickers = {
+        colorscheme = {
+          enable_preview = true
+        }
+      }
+    }
+  end
 }
