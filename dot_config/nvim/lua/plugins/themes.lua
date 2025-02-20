a9ffb51741748773b7396e6bc2fa775591e5f055
Aleksy Rudy <aleksy@aleksyrudy.pl>
2025-02-20 13:46:15 +0100

    Update .config/nvim/lua/plugins/themes.lua

--- dot_config/nvim/lua/plugins/themes.lua
+++ dot_config/nvim/lua/plugins/themes.lua
@@ -1,5 +1,5 @@
 return {
-  { "bluz71/vim-nightfly-colors", name = "nightfly",   lazy = false, priority = 1000 },
-  { "EdenEast/nightfox.nvim",     name = "nightfox",   lazy = false, priority = 1000 },
-  { "folke/tokyonight.nvim",      name = "tokyonight", lazy = false, priority = 1000 }
+  { "bluz71/vim-nightfly-colors", name = "theme-nightfly",   lazy = false, priority = 1000 },
+  { "EdenEast/nightfox.nvim",     name = "theme-nightfox",   lazy = false, priority = 1000 },
+  { "folke/tokyonight.nvim",      name = "theme-tokyonight", lazy = false, priority = 1000 }
 }
