a6b1419d4cbdf85481035f10da3ff10d635ce1ee
Aleksy Rudy <DanzigerGeist@icloud.com>
2025-02-21 15:49:57 +0100

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,20 @@
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-ui-select.nvim',
+    'nvim-telescope/telescope-file-browser.nvim'
   },
   config = function()
-    require "telescope".setup {
+    local telescope = require('telescope')
+    telescope.setup {
       pickers = {
         colorscheme = {
           enable_preview = true
         }
       }
     }
+    telescope.load_extension("file_browser")
+    telescope.load_extension("ui-select")
   end
 }
