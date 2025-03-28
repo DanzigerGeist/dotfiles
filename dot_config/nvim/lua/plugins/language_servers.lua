725d21142369584b847045fc2ee73ce5ade3b499
Aleksy Rudy <DanzigerGeist@icloud.com>
2025-03-28 10:21:39 +0100

    Update .config/nvim/lua/plugins/language_servers.lua

--- dot_config/nvim/lua/plugins/language_servers.lua
+++ dot_config/nvim/lua/plugins/language_servers.lua
@@ -3,24 +3,22 @@ local treesitter = {
   dependencies = { "windwp/nvim-ts-autotag" },
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      ensure_installed = {
-        "bash", "c", "cmake", "comment", "cpp", "css", "csv", "diff", "dockerfile",
-        "dot", "erlang", "git_config", "git_rebase", "gitattributes", "gitcommit",
-        "gitignore", "go", "gomod", "gosum", "gotmpl", "gowork", "groovy", "hcl",
-        "helm", "html", "http", "java", "javascript", "jq", "json", "json5", "kotlin",
-        "lua", "luadoc", "make", "markdown", "markdown_inline", "mermaid", "muttrc",
-        "nginx", "pem", "perl", "php", "powershell", "properties", "python", "regex",
-        "ruby", "rust", "scala", "sql", "ssh_config", "swift", "tcl", "terraform",
-        "toml", "typescript", "tmux", "vim", "vimdoc", "vue", "xml", "yaml"
-      },
-      autotag = { enable = true },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  opts = {
+    auto_install = true,
+    ensure_installed = {
+      "bash", "c", "cmake", "comment", "cpp", "css", "csv", "diff", "dockerfile",
+      "dot", "erlang", "git_config", "git_rebase", "gitattributes", "gitcommit",
+      "gitignore", "go", "gomod", "gosum", "gotmpl", "gowork", "groovy", "hcl",
+      "helm", "html", "http", "java", "javascript", "jq", "json", "json5", "kotlin",
+      "lua", "luadoc", "make", "markdown", "markdown_inline", "mermaid", "muttrc",
+      "nginx", "pem", "perl", "php", "powershell", "properties", "python", "regex",
+      "ruby", "rust", "scala", "sql", "ssh_config", "swift", "tcl", "terraform",
+      "toml", "typescript", "tmux", "vim", "vimdoc", "vue", "xml", "yaml"
+    },
+    autotag = { enable = true },
+    highlight = { enable = true },
+    indent = { enable = true },
+  }
 }
 
 local mason = {
