d5dc3c708d9609925a2ed6a4a8f377663d1ff3dc
Aleksy Rudy <DanzigerGeist@icloud.com>
2025-03-28 10:13:54 +0100

    Update .config/nvim/lua/plugins/interface.lua

--- dot_config/nvim/lua/plugins/interface.lua
+++ dot_config/nvim/lua/plugins/interface.lua
@@ -1,60 +1,68 @@
 local lualine = {
   "nvim-lualine/lualine.nvim",
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local theme = function()
-      local colors = {
-        darkgray = "#16161d",
-        gray = "#727169",
-        outerbg = "#16161D",
-        normal = "#7aa2f7",  -- Tokyonight blue
-        insert = "#9ece6a",  -- Tokyonight green
-        visual = "#bb9af7",  -- Tokyonight purple
-        replace = "#f7768e", -- Tokyonight red
-        command = "#e0af68", -- Tokyonight yellow
-      }
-      return {
-        inactive = {
-          a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-        visual = {
-          a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-        replace = {
-          a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-        normal = {
-          a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-        insert = {
-          a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-        command = {
-          a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
-          b = { fg = colors.gray, bg = colors.outerbg },
-          c = { fg = colors.gray, bg = "none" },
-        },
-      }
-    end
-    require('lualine').setup({
-      options = {
-        theme = theme(),
-        icons_enabled = true,
-        section_separators = { left = "", right = "" },
-      },
-      extensions = { 'trouble', 'quickfix' },
-    })
-  end
+  opts = {
+    options = {
+      theme = theme(),
+      icons_enabled = true,
+      section_separators = { left = "", right = "" },
+    },
+    extensions = { 'trouble', 'quickfix' }
+  }
+  -- config = function()
+  --   local theme = function()
+  --     local colors = {
+  --       darkgray = "#16161d",
+  --       gray = "#727169",
+  --       outerbg = "#16161D",
+  --       normal = "#7aa2f7",  -- Tokyonight blue
+  --       insert = "#9ece6a",  -- Tokyonight green
+  --       visual = "#bb9af7",  -- Tokyonight purple
+  --       replace = "#f7768e", -- Tokyonight red
+  --       command = "#e0af68", -- Tokyonight yellow
+  --     }
+  --     return {
+  --       inactive = {
+  --         a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --       visual = {
+  --         a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --       replace = {
+  --         a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --       normal = {
+  --         a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --       insert = {
+  --         a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --       command = {
+  --         a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+  --         b = { fg = colors.gray, bg = colors.outerbg },
+  --         c = { fg = colors.gray, bg = "none" },
+  --       },
+  --     }
+  --   end
+  --   require('lualine').setup({
+  --     options = {
+  --       theme = theme(),
+  --       icons_enabled = true,
+  --       section_separators = { left = "", right = "" },
+  --     },
+  --     extensions = { 'trouble', 'quickfix' },
+  --   })
+  -- end
 }
 
 local noice = {
@@ -97,35 +105,26 @@ local tokyonight = {
       floats = "transparent",
     }
   },
-  config = function(_, opts)
-    require('tokyonight').setup(opts)
-    vim.cmd('colorscheme tokyonight')
-    -- vim.api.nvim_set_hl(0, "CursorLine", { underline = true, bg = "none" })
-    -- vim.api.nvim_set_hl(0, "PmenuSel", { underline = true, bg = "none", fg = "none" })
-  end
 }
 
 local bufferline = {
   'akinsho/bufferline.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local bufferline = require('bufferline')
-    bufferline.setup {
-      options = {
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(_, _, diagnostics_dict, _)
-          local s = " "
-          for e, n in pairs(diagnostics_dict) do
-            local sym = e == "error" and " "
-                or (e == "warning" and " " or " ")
-            s = s .. n .. sym
-          end
-          return s
-        end,
-        mode = 'buffers',
-      },
+  opts = {
+    options = {
+      diagnostics = 'nvim_lsp',
+      diagnostics_indicator = function(_, _, diagnostics_dict, _)
+        local s = " "
+        for e, n in pairs(diagnostics_dict) do
+          local sym = e == "error" and " "
+              or (e == "warning" and " " or " ")
+          s = s .. n .. sym
+        end
+        return s
+      end,
+      mode = 'buffers',
     }
-  end
+  }
 }
 
 return { lualine, noice, bufferline, tokyonight }
