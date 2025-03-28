6916060c314955ddf8c74a9fa1c2b2cdd1bad194
Aleksy Rudy <DanzigerGeist@icloud.com>
2025-03-28 10:09:43 +0100

    Update .config/nvim/lua/plugins/interface.lua

--- dot_config/nvim/lua/plugins/interface.lua
+++ dot_config/nvim/lua/plugins/interface.lua
@@ -110,22 +110,21 @@ local bufferline = {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     local bufferline = require('bufferline')
-    bufferline.setup({})
-    -- bufferline.setup {
-    --   options = {
-    --     diagnostics = 'nvim_lsp',
-    --     diagnostics_indicator = function(_, _, diagnostics_dict, _)
-    --       local s = " "
-    --       for e, n in pairs(diagnostics_dict) do
-    --         local sym = e == "error" and " "
-    --             or (e == "warning" and " " or " ")
-    --         s = s .. n .. sym
-    --       end
-    --       return s
-    --     end,
-    --     mode = 'buffers',
-    --   },
-    -- }
+    bufferline.setup {
+      options = {
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(_, _, diagnostics_dict, _)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " "
+                or (e == "warning" and " " or " ")
+            s = s .. n .. sym
+          end
+          return s
+        end,
+        mode = 'buffers',
+      },
+    }
   end
 }
 
