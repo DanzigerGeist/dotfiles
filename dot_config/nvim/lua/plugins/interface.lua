d1232a1f9846021c49b674348ddce4c832510631
Aleksy Rudy <DanzigerGeist@icloud.com>
2025-03-28 10:07:17 +0100

    Update .config/nvim/lua/plugins/interface.lua

--- dot_config/nvim/lua/plugins/interface.lua
+++ dot_config/nvim/lua/plugins/interface.lua
@@ -108,24 +108,24 @@ local tokyonight = {
 local bufferline = {
   'akinsho/bufferline.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local bufferline = require('bufferline')
-    bufferline.setup {
-      options = {
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(_, _, diagnostics_dict, _)
-          local s = " "
-          for e, n in pairs(diagnostics_dict) do
-            local sym = e == "error" and " "
-                or (e == "warning" and " " or " ")
-            s = s .. n .. sym
-          end
-          return s
-        end,
-        mode = 'buffers',
-      },
-    }
-  end
+  -- config = function()
+  --   local bufferline = require('bufferline')
+  --   bufferline.setup {
+  --     options = {
+  --       diagnostics = 'nvim_lsp',
+  --       diagnostics_indicator = function(_, _, diagnostics_dict, _)
+  --         local s = " "
+  --         for e, n in pairs(diagnostics_dict) do
+  --           local sym = e == "error" and " "
+  --               or (e == "warning" and " " or " ")
+  --           s = s .. n .. sym
+  --         end
+  --         return s
+  --       end,
+  --       mode = 'buffers',
+  --     },
+  --   }
+  -- end
 }
 
 return { lualine, noice, bufferline, tokyonight }
